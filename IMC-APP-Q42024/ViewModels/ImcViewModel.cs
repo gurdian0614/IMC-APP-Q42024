9f03b22fd11a0a364836ee30404c0ea50a46d1cd
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-06 19:13:52 -0600

    Adding code

--- IMC-APP-Q42024/ViewModels/ImcViewModel.cs
+++ IMC-APP-Q42024/ViewModels/ImcViewModel.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Runtime.CompilerServices;
 
 namespace IMC_APP_Q42024.ViewModels
 {
-    internal class ImcViewModel
+    public partial class ImcViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private double peso;
+
+        [ObservableProperty]
+        private double estatura;
+
+        [ObservableProperty]
+        private double imc;
+
+        private void Alerta(string Titulo, string Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+        }
+
+        [RelayCommand]
+        private void Calcular()
+        {
+            try
+            {
+                if (Peso <= 0)
+                {
+                    Alerta("ADVERTENCIA", "Valor del peso no válido");
+                }
+                else if (Estatura <= 0)
+                {
+                    Alerta("ADVERTENCIA", "Valor de la estatura no válida");
+                }
+                else
+                {
+                    Imc = Peso / (Estatura * Estatura);
+                }
+            }
+            catch (Exception ex)
+            {
+                Alerta("ERROR", ex.Message);
+            }
+        }
+
+        [RelayCommand]
+        private void Limpiar()
+        {
+            Peso = 0;
+            Estatura = 0;
+            Imc = 0;
+        }
     }
 }
--- IMC-APP-Q42024/Views/ImcView.xaml.cs
+++ IMC-APP-Q42024/Views/ImcView.xaml.cs
@@ -1,9 +1,14 @@
+using IMC_APP_Q42024.ViewModels;
+
 namespace IMC_APP_Q42024.Views;
 
 public partial class ImcView : ContentPage
 {
+	ImcViewModel viewModel;
 	public ImcView()
 	{
 		InitializeComponent();
+		viewModel = new ImcViewModel();
+		BindingContext = viewModel;
 	}
 }
