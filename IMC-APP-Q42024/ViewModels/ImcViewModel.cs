e5641239defaa5ec4626323174457cc7a2f20978
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-09 18:54:16 -0600

    Adding imc message

--- IMC-APP-Q42024/ViewModels/ImcViewModel.cs
+++ IMC-APP-Q42024/ViewModels/ImcViewModel.cs
@@ -14,7 +14,7 @@ namespace IMC_APP_Q42024.ViewModels
         private double estatura;
 
         [ObservableProperty]
-        private double imc;
+        private string resultado;
 
         private void Alerta(string Titulo, string Mensaje)
         {
@@ -24,6 +24,8 @@ namespace IMC_APP_Q42024.ViewModels
         [RelayCommand]
         private void Calcular()
         {
+            double imc = 0;
+
             try
             {
                 if (Peso <= 0)
@@ -36,7 +38,8 @@ namespace IMC_APP_Q42024.ViewModels
                 }
                 else
                 {
-                    Imc = Peso / (Estatura * Estatura);
+                    imc = Peso / (Estatura * Estatura);
+                    Resultado = $"{Math.Round(imc, 1).ToString()} - {mensajeImc(imc)}";
                 }
             }
             catch (Exception ex)
@@ -50,7 +53,29 @@ namespace IMC_APP_Q42024.ViewModels
         {
             Peso = 0;
             Estatura = 0;
-            Imc = 0;
+            Resultado = "";
+        }
+
+        private string mensajeImc(double imc)
+        {
+            string mensaje = "";
+
+            if (imc < 18.5)
+            {
+                mensaje = "Bajo peso";
+            } else if(imc >= 18.5 && imc <= 24.9)
+            {
+                mensaje = "Peso normal";
+            }
+            else if (imc >= 25 && imc <= 29.9)
+            {
+                mensaje = "Sobrepeso";
+            } else
+            {
+                mensaje = "Obesidad";
+            }
+
+            return mensaje;
         }
     }
 }
